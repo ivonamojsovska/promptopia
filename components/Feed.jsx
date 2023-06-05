effd9577982468a4ac0b796ce0ba647194c0c825
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-06-05 15:01:50 -0700

    handletag deleted

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,15 +2,11 @@
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
-const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
+        <PromptCard key={post._id} post={post} />
       ))}
     </div>
   );
@@ -67,10 +63,7 @@ const Feed = () => {
         ></input>
       </form>
 
-      <PromptCardList
-        data={searchText ? searchedResults : posts}
-        handleTagClick={() => {}}
-      />
+      <PromptCardList data={searchText ? searchedResults : posts} />
     </section>
   );
 };
